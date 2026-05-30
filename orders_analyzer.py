1ec2e2b483627b1fbc66a94cb79a6fbddc9a2f7b
Michał Bugajski <michabugajski2@gmail.com>
2026-05-31 00:59:52 +0200

    Group metrics and top results into dict

--- orders_analyzer.py
+++ orders_analyzer.py
@@ -1,5 +1,4 @@
 import csv
-import re
 import sys
 
 def find_top_item(data):
@@ -44,7 +43,7 @@ def load_orders(filename):
         for row in reader:
             for column in required_columns:
                 if row[column].strip() == "":
-                    raise ValueError(f"Error: missing value in {column}")
+                    raise ValueError(f"Missing value in {column}")
             
                
             order_id = int(row["order_id"])
@@ -58,10 +57,10 @@ def load_orders(filename):
             country = row["country"]
 
             if quantity <= 0:
-                raise ValueError("Error: quantity cannot be negative or zero.")                
+                raise ValueError("Quantity cannot be negative or zero.")                
 
             if price < 0:
-                raise ValueError("Error: price cannot be negative.")
+                raise ValueError("Price cannot be negative.")
                 
             order = {
                 "order_id": order_id,
@@ -122,55 +121,79 @@ def calculate_metrics(orders):
 
     average_order_value = total_revenue / total_orders
 
-    return total_revenue, total_orders, average_order_value, revenue_by_customer, orders_by_customer, unique_customers, revenue_by_country, revenue_by_category
+    metrics = {
+        "total_revenue": total_revenue,
+        "total_orders": total_orders,
+        "average_order_value": average_order_value,
+        "revenue_by_customer": revenue_by_customer,
+        "orders_by_customer": orders_by_customer,
+        "unique_customers": unique_customers,
+        "revenue_by_country": revenue_by_country,
+        "revenue_by_category": revenue_by_category
+    }
+
+    return metrics
+
+def calculate_top_results(metrics):
+    top_results = {}
+
+    highest_revenue_customer_name, highest_revenue_customer_value = find_top_item(metrics["revenue_by_customer"])
+    top_results["revenue_by_customer"] = {
+        "name": highest_revenue_customer_name,
+        "value": highest_revenue_customer_value
+    }
+
+    top_orders_customer_name, top_orders_customer_value = find_top_item(metrics["orders_by_customer"])   
+    top_results["orders_by_customer"] = {
+        "name": top_orders_customer_name,
+        "value": top_orders_customer_value
+    }
+
+    top_revenue_country_name, top_revenue_country_value = find_top_item(metrics["revenue_by_country"])
+    top_results["revenue_by_country"] = {
+        "name": top_revenue_country_name,
+        "value": top_revenue_country_value
+    }
+
+    top_revenue_category_name, top_revenue_category_value = find_top_item(metrics["revenue_by_category"])
+    top_results["revenue_by_category"] = {
+        "name": top_revenue_category_name,
+        "value": top_revenue_category_value
+    }
+
+    return top_results
 
 
-def print_report(total_revenue,
-                total_orders,
-                average_order_value,
-                revenue_by_customer,
-                orders_by_customer,
-                unique_customers,
-                revenue_by_country,
-                revenue_by_category,
-                highest_revenue_customer_name,
-                highest_revenue_customer_value,
-                top_orders_customer_name,
-                top_orders_customer_value,
-                top_revenue_country_name,
-                top_revenue_country_value,
-                top_revenue_category_name, 
-                top_revenue_category_value
-    ):
+def print_report(metrics, top_results):
 
     print("Revenue by customer: ")
-    for name in revenue_by_customer:
-        print(f"- {name}: {format_money(revenue_by_customer[name])} PLN")
+    for name in metrics["revenue_by_customer"]:
+        print(f"- {name}: {format_money(metrics['revenue_by_customer'][name])} PLN")
     print()
     print("Orders by customer: ")
-    for name in orders_by_customer:
-        if orders_by_customer[name] == 1:
-            print(f"- {name}: {orders_by_customer[name]} order")
+    for name in metrics["orders_by_customer"]:
+        if metrics["orders_by_customer"][name] == 1:
+            print(f"- {name}: {metrics['orders_by_customer'][name]} order")
         else:
-            print(f"- {name}: {orders_by_customer[name]} orders")
+            print(f"- {name}: {metrics['orders_by_customer'][name]} orders")
     print()
     print("Revenue by country: ")
-    for country in revenue_by_country:
-        print(f"- {country}: {format_money(revenue_by_country[country])} PLN")
+    for country in metrics["revenue_by_country"]:
+        print(f"- {country}: {format_money(metrics['revenue_by_country'][country])} PLN")
     print()
     print("Revenue by category: ")
-    for category in revenue_by_category:
-        print(f"- {category}: {format_money(revenue_by_category[category])} PLN")
+    for category in metrics["revenue_by_category"]:
+        print(f"- {category}: {format_money(metrics['revenue_by_category'][category])} PLN")
 
     print()
-    print(f"Total revenue: {format_money(total_revenue)} PLN")
-    print(f"Total orders: {total_orders}")
-    print(f"Average order value: {format_money(average_order_value)} PLN")
-    print(f"Customer with most orders: {top_orders_customer_name} - {top_orders_customer_value} orders.")
-    print(f"Customer with highest revenue: {highest_revenue_customer_name} - {format_money(highest_revenue_customer_value)} PLN")
-    print(f"Country with highest revenue: {top_revenue_country_name} - {format_money(top_revenue_country_value)} PLN")
-    print(f"Category with highest revenue: {top_revenue_category_name} - {format_money(top_revenue_category_value)} PLN")
-    print(f"Unique customers: {len(unique_customers)}")
+    print(f"Total revenue: {format_money(metrics['total_revenue'])} PLN")
+    print(f"Total orders: {metrics['total_orders']}")
+    print(f"Average order value: {format_money(metrics['average_order_value'])} PLN")
+    print(f"Customer with most orders: {top_results['orders_by_customer']['name']} - {top_results['orders_by_customer']['value']} orders.")
+    print(f"Customer with highest revenue: {top_results['revenue_by_customer']['name']} - {format_money(top_results['revenue_by_customer']['value'])} PLN")
+    print(f"Country with highest revenue: {top_results['revenue_by_country']['name']} - {format_money(top_results['revenue_by_country']['value'])} PLN")
+    print(f"Category with highest revenue: {top_results['revenue_by_category']['name']} - {format_money(top_results['revenue_by_category']['value'])} PLN")
+    print(f"Unique customers: {len(metrics['unique_customers'])}")
 
 def main():
 
@@ -197,34 +220,11 @@ def main():
         print("Error: CSV file contains no order data.")
         exit()
 
-    total_revenue, total_orders, average_order_value, revenue_by_customer, orders_by_customer, unique_customers, revenue_by_country, revenue_by_category = calculate_metrics(orders)
+    metrics = calculate_metrics(orders)
+    top_results = calculate_top_results(metrics)
 
-    highest_revenue_customer_name, highest_revenue_customer_value = find_top_item(revenue_by_customer)
-
-    top_orders_customer_name, top_orders_customer_value = find_top_item(orders_by_customer)
-
-    top_revenue_country_name, top_revenue_country_value = find_top_item(revenue_by_country)
-
-    top_revenue_category_name, top_revenue_category_value = find_top_item(revenue_by_category)
-
-    print_report(total_revenue,
-                    total_orders,
-                    average_order_value,
-                    revenue_by_customer,
-                    orders_by_customer,
-                    unique_customers,
-                    revenue_by_country,
-                    revenue_by_category,
-                    highest_revenue_customer_name,
-                    highest_revenue_customer_value,
-                    top_orders_customer_name,
-                    top_orders_customer_value,
-                    top_revenue_country_name,
-                    top_revenue_country_value,
-                    top_revenue_category_name, 
-                    top_revenue_category_value
-    )
-
+    print_report(metrics, top_results)
+    
     
 if __name__ == "__main__":
     main()
